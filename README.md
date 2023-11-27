f213132d25f005d155c4f648085ff52817031c74
PONEBU <polzovatel@192.168.1.7>
2023-11-27 19:29:37 +0100

    init logger

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # urt_shortener_restapi
+lol ok im learning thins what a u waiting for here xd
--- cmd/url_shortener/main.go
+++ cmd/url_shortener/main.go
@@ -1,23 +1,52 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"example.com/m/v2/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	// TODO: init config: cleanenv
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
-
 	// TODO: init logger: slog
+	log := setupLogger(cfg.Env)
 
+	log.Info("Starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 	// TODO: init storage: sqlite
 
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run server:
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
